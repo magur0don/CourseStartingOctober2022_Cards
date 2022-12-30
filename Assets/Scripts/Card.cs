e88a610692f5ee44762541f7bbf556b8731a940a
KotaShogo <>
2022-12-30 17:36:25 +0900

    トランプのカードの基本情報クラスの作成と、後々使うトランプの画像を追加

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// トランプのカードの基本情報
+/// </summary>
+public class Card
+{
+    // 柄
+    public enum Suit
+    {
+        Invalid = -1,
+        Club,
+        Dia,
+        Heart,
+        Spade,
+        Max
+    }
+
+    /// <summary>
+    /// トランプの柄
+    /// </summary>
+    public Suit CardSuit = Suit.Invalid;
+
+    /// <summary>
+    /// トランプの数字
+    /// </summary>
+    public int Number = 0;
+
+    /// <summary>
+    /// カードの初期化
+    /// </summary>
+    /// <param name="suit">柄</param>
+    /// <param name="number">トランプの数字</param>
+    public Card(Suit suit, int number)
+    {
+        this.CardSuit = suit;
+        this.Number = number;
+    }
+}
