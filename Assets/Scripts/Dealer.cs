cf4e2d702b0d999ed3ce1a84e3a2726533033c06
KotaShogo <>
2022-12-31 22:40:40 +0900

    linqの例を追加

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -11,5 +11,17 @@ public class Dealer : MonoBehaviour
         Deck.GetDeck();
         Debug.Log($"スート：{Deck.CardDeck.FirstOrDefault().CardSuit} " +
             $"数字：{Deck.CardDeck.FirstOrDefault().Number}");
+
+        // Linqにおける例：where
+        // ラムダ式でboolを判定し、List内に判定条件に合致する要素を返す
+        var clubCards = Deck.CardDeck.Where(card => card.CardSuit == Card.Suit.Club).ToList();
+        var clubOne = clubCards.FirstOrDefault();
+        // clubの1
+
+
+        // Linqにおける例：any
+        // ラムダ式でboolを判定し、List内に判定条件に合致するかtrueかflaseで返す
+        var clubCardsInHeartCard = Deck.CardDeck.Any(card => card.CardSuit == Card.Suit.Heart);
+        // false
     }
 }
