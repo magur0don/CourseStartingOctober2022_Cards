8e2af9f227ec9bd5611e0fc95d7c3729c93be9a2
KotaShogo <>
2022-12-31 14:30:00 +0900

    ディーラークラスを作成し、Deckを作成してみる

---
+++ Assets/Scripts/Dealer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Dealer : MonoBehaviour
+{
+    private Deck Deck = new Deck(); 
+
+    private void Start()
+    {
+        Deck.GetDeck();
+        Debug.Log($"スート：{Deck.CardDeck.FirstOrDefault().CardSuit} " +
+            $"数字：{Deck.CardDeck.FirstOrDefault().Number}");
+    }
+}
