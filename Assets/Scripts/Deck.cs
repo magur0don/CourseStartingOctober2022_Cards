3d04d3ee365d3d62b4d98f806c3cffa17ff1bacd
KotaShogo <>
2022-12-31 14:20:31 +0900

    Deckを作成する

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Deck
+{
+    /// <summary>
+    /// トランプのカードデック
+    /// </summary>
+    public List<Card> CardDeck = new List<Card>();
+
+    /// <summary>
+    /// 昇順のDeckをゲットする
+    /// </summary>
+    public List<Card> GetDeck()
+    {
+        // 一度作られたデッキがある場合はCardDeckを返す
+        if (CardDeck.FirstOrDefault() != null) {
+            return CardDeck;
+        }
+
+        // デッキがない場合はここでデッキを作成する
+        for (int i = 0; i < CardHelper.CardMax; i++)
+        {
+            CardDeck.Add(new Card(CardHelper.CardSuitJudge(i), CardHelper.CardNumJudge(i)));
+        }
+        return CardDeck;
+    }
+}
