3fe3c6b62d81d71c127b2edfcd96e8d80651e417
KotaShogo <>
2023-01-12 22:05:09 +0900

    カードを1枚描画する

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class Dealer : MonoBehaviour
 {
-    private Deck Deck = new Deck(); 
+    private Deck Deck = new Deck();
+
+    [SerializeField]
+    private SpriteAtlas CardAtlas;
+
+    // トランプを表示するImage
+    public Image card;
 
     private void Start()
     {
         Deck.GetDeck();
-        Debug.Log($"スート：{Deck.CardDeck.FirstOrDefault().CardSuit} " +
-            $"数字：{Deck.CardDeck.FirstOrDefault().Number}");
 
         // Linqにおける例：where
         // ラムダ式でboolを判定し、List内に判定条件に合致する要素を返す
         var clubCards = Deck.CardDeck.Where(card => card.CardSuit == Card.Suit.Club).ToList();
         var clubOne = clubCards.FirstOrDefault();
         // clubの1
-
-
         // Linqにおける例：any
         // ラムダ式でboolを判定し、List内に判定条件に合致するかtrueかflaseで返す
         var clubCardsInHeartCard = Deck.CardDeck.Any(card => card.CardSuit == Card.Suit.Heart);
         // false
+
+        // カードを文字列をフックに表示する
+        card.sprite = CardAtlas.GetSprite($"Card_{((int)clubOne.CardSuit * 13) + clubOne.Number - 1}");
     }
 }
